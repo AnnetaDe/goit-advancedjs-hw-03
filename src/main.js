350c7a3c6c54af43521b7148e1895153eab06cec
AnnetaDe <anneta.liss@gmail.com>
2025-06-23 13:02:03 +0200

    upd

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-// import "./js/pixabay-api.js";
-// import "./js/render-functions.js";
+
 
 import { onSearchFormSubmit } from "./js/render-functions.js";
 searchForm.addEventListener("submit", onSearchFormSubmit);
