d713279464ab09a6b5067e560765366a68783452
AnnetaDe <anneta.liss@gmail.com>
2025-06-23 13:09:30 +0200

    upd

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -102,37 +102,4 @@ function getImagesByQuery(query) {
     });
 }
 
-export const onSearchFormSubmit = event => {
-  event.preventDefault();
-  const query = event.target.elements.picture_search.value.trim();
-
-  if (!checkQuery({ query })) {
-    return;
-  }
-  showLoader();
-  clearGallery();
-  getImagesByQuery(query)
-    .then(images => {
-      if (images.length > 0) {
-        renderGallery(images);
-        showToast(`Found ${images.length} images`, "success");
-        let lightbox;
-        if (!lightbox) {
-          lightbox = new SimpleLightbox(".gallery a", {
-            captionsData: "alt",
-            captionDelay: 250,
-          });
-        } else {
-          lightbox.refresh();
-        }
-    
-      } else {
-        showToast("No images found", "error");
-      }
-    })
-    .finally(() => {
-      hideLoader();
-    });
- 
-};
-searchForm.addEventListener("submit", onSearchFormSubmit);
+export { renderGallery, clearGallery, showLoader, hideLoader, showToast, checkQuery, getImagesByQuery };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,41 @@
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { renderGallery, clearGallery, showLoader, hideLoader, showToast, checkQuery, getImagesByQuery } from "./js/render-functions.js";
 
-import { onSearchFormSubmit } from "./js/render-functions.js";
+export const onSearchFormSubmit = event => {
+    event.preventDefault();
+    const query = event.target.elements.picture_search.value.trim();
+  
+    if (!checkQuery({ query })) {
+      return;
+    }
+    showLoader();
+    clearGallery();
+    getImagesByQuery(query)
+      .then(images => {
+        if (images.length > 0) {
+          renderGallery(images);
+          showToast(`Found ${images.length} images`, "success");
+          let lightbox;
+          if (!lightbox) {
+            lightbox = new SimpleLightbox(".gallery a", {
+              captionsData: "alt",
+              captionDelay: 250,
+            });
+          } else {
+            lightbox.refresh();
+          }
+      
+        } else {
+          showToast("No images found", "error");
+        }
+      })
+      .finally(() => {
+        hideLoader();
+      });
+   
+  };
+
+const searchForm = document.querySelector(".form");
 searchForm.addEventListener("submit", onSearchFormSubmit);
